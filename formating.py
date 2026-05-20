49862085c6d19c1ed586a212afe7b108cf0e4aea
Satyam2OO6 <sdas27092006@gmail.com>
2026-05-20 10:30:46 +0530

    Add files via upload

---
+++ formating.py
@@ -0,0 +1,7 @@
+name,age="sat",20
+massage= "Hello! i'm %s and my age is %d" % (name,age)
+massage1= f"Hello! i'm {name} and my age is {age}" 
+massage2= "Hello! i'm {} and i'm {}" .format(name,age)
+print(massage)
+print(massage1)
+print(massage2)
