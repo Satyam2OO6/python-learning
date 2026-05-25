45708d8452161d01ebd21da5f81c69994ed08d50
Satyam2OO6 <sdas27092006@gmail.com>
2026-05-25 09:27:01 +0530

    Add files via upload

---
+++ arg.py
@@ -0,0 +1,17 @@
+# a = "how-are-you"
+# print(a.split())
+
+
+# def mean(*args):
+#     return sum(args)/len(args)
+
+
+# print(mean(2,4,5,15,7,8,8))
+
+
+def mean(**kwargs):
+    return kwargs
+
+
+
+print(mean(a=37,b=7,c=58,d=8))    
