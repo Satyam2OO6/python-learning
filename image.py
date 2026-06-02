89ca020c0c4945cf60668a70ee70709ec7a303e5
Satyam2OO6 <sdas27092006@gmail.com>
2026-06-02 21:13:54 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,12 @@
+import cv2
+img=cv2.imread("peace.jpg",0)
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resized=cv2.resize(img,(int(img.shape[1]//2),int(img.shape[0]/2)))
+cv2.imshow("Peace",resized)
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
+
